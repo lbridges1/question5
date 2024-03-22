c770107b8cbb265118a68d486d338285aa1cc7b9
lbridges1 <155769050+lbridges1@users.noreply.github.com>
2024-03-22 11:28:29 -0700

    Create main.py

---
+++ main.py
@@ -0,0 +1,52 @@
+import os
+import base64
+from typing import Union
+from os.path import dirname, abspath, join
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+
+current_dir = dirname(abspath(__file__))
+static_path = join(current_dir, "static")
+
+app = FastAPI()
+app.mount("/ui", StaticFiles(directory=static_path), name="ui")
+
+
+class Body(BaseModel):
+    length: Union[int, None] = 20
+
+
+@app.get('/')
+def root():
+    html_path = join(static_path, "index.html")
+    return FileResponse(html_path)
+
+
+@app.post('/generate')
+def generate(body: Body):
+    """
+    Generate a pseudo-random token ID of twenty characters by default. Example POST request body:
+
+    {
+        "length": 20
+    }
+    """
+    string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
+
+    return {"token": string}
+
+
+class Text(BaseModel):
+    text: str
+
+# Create a Pydantic model #
+# Create a FastAPI endpoint that accepts a POST request with a JSON body containing a single field called "text" and returns a checksum of the text 
+# using the SHA-256 algorithm.
+@app.post('/checksum')
+def checksum(text: Text):
+    import hashlib
+    sha256 = hashlib.sha256()
+    sha256.update(text.text.encode())
+    return {"checksum": sha256.hexdigest()}
